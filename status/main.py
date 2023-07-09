4c0644d5afe24adc248b3a9bdb674e749464718c
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-07-09 22:08:39 +0300

    added logic to methods

--- status/main.py
+++ status/main.py
@@ -1,13 +1,33 @@
+from collections import defaultdict
+
+
 class TreeStore:
+    def __init__(self, items) -> None:
+        self.items = {item['id']: item for item in items}
+        self.children = defaultdict(list)
+        self.parents = {}
+        for item in items:
+            if (parent_id := item['parent']) is not None:
+                self.children[parent_id].append(item)
+            self.parents[item['id']] = parent_id
 
     def getAll(self) -> list:
-        pass
+        return list(self.items.values())
 
-    def getItem(self, item_id, default=None) -> dict:
-        pass
+    def getItem(self, item_id) -> dict:
+        return self.items.get(item_id)
 
     def getChildren(self, parent_id) -> list:
-        pass
+        return self.children.get(parent_id, [])
 
     def getAllParents(self, item_id) -> list:
-        pass
+        parent_id = self.parents.get(item_id)
+        parents = []
+        while parent_id is not None:
+            parent = self.items.get(parent_id)
+            if parent is not None:
+                parents.append(parent)
+                parent_id = parent['parent']
+            else:
+                break
+        return parents
--- tests/test_file.py
+++ tests/test_file.py
@@ -4,6 +4,16 @@ from status.main import TreeStore
 
 
 class TestTreeStore:
+    items = [
+            {'id': 1, 'parent': 'root'},
+            {'id': 2, 'parent': 1, 'type': 'test'},
+            {'id': 3, 'parent': 1, 'type': 'test'},
+            {'id': 4, 'parent': 2, 'type': 'test'},
+            {'id': 5, 'parent': 2, 'type': 'test'},
+            {'id': 6, 'parent': 2, 'type': 'test'},
+            {'id': 7, 'parent': 4, 'type': None},
+            {'id': 8, 'parent': 4, 'type': None}
+    ]
 
     def test_getAll(self):
         expected = [
@@ -16,19 +26,20 @@ class TestTreeStore:
             {'id': 7, 'parent': 4, 'type': None},
             {'id': 8, 'parent': 4, 'type': None},
         ]
-        assert TreeStore().getAll() == expected
+
+        assert TreeStore(self.items).getAll() == expected
 
     def test_getItem(self):
         expected = {'id': 7, 'parent': 4, 'type': None}
-        assert TreeStore().getItem(7) == expected
+        assert TreeStore(self.items).getItem(7) == expected
 
     def test_getChildren(self):
         expected = [
             {'id': 7, 'parent': 4, 'type': None},
             {'id': 8, 'parent': 4, 'type': None},
         ]
-        assert TreeStore().getChildren(4) == expected
-        assert TreeStore().getChildren(5) == []
+        assert TreeStore(self.items).getChildren(4) == expected
+        assert TreeStore(self.items).getChildren(5) == []
 
     def test_getAllParents(self):
         expected = [
@@ -36,7 +47,7 @@ class TestTreeStore:
             {'id': 2, 'parent': 1, 'type': 'test'},
             {'id': 1, 'parent': 'root'},
         ]
-        assert TreeStore().getAllParents(7) == expected
+        assert TreeStore(self.items).getAllParents(7) == expected
 
 
 if __name__ == '__main__':
